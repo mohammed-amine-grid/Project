13f26329a1844723b20d34913d8f5b3dad4dedf3
Greedz <jakommag@gmail.com>
2020-10-19 21:11:41 +0100

    h

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-const numbers = document.getElementsByClassName('number')
+const numbers = $('.number')
+const clear = $('.clear')
+const operator = $('.operator')
+const display = $('.display')
+const decimal = $('dec')
+
+var displayVal = 0;
+var previous;
+var evalData = [];
+
+
+for (var i = 0; i < numbers.length; i++) {
+
+}
