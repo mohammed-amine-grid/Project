d5f62c37395d48c5fd7c94b5c99f39be83f18fe3
Greedz <jakommag@gmail.com>
2020-10-20 05:14:54 +0100

    yikes

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
-var input = document.getElementById('input')
-var operator = document.querySelectorAll('.operators div')
-var result = document.getElementById('result')
-var clear = document.getElementById('clear') 
+$('.btn').click(function () {
+    var first_num = $('.calculate_field').val()
+    var number = $(this).html();
+    first_num += number; $('.calculate_field').val(first_num);
+})
